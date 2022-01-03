884211bec0e7a479d16ac9a432af4215a0113425
jo_kil <jo.kil.plays@gmail.com>
2022-01-03 14:12:13 +0100

    route tests

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+mod data_interface;
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/cityproject/<cp_id>")]
+fn cityproject(cp_id: i64) -> String {
+    "requested cityproject id ".to_string() + &cp_id.to_string()
+}
+
+#[get("/cityprojects")]
+fn cityprojects() -> String {
+    "requested a list of all cityprojects".to_string()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![cityproject, cityprojects])
 }
