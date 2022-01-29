9884308d4f15b79fbf7a91c0e01fb148e6fd3557
jo_kil <jo.kil.plays@gmail.com>
2022-01-29 14:06:19 +0100

    delete unused test projects

--- tests/sea_orm_plotsystem_tables/migrations/20220118145000_setup.sql
+++
@@ -1,8 +0,0 @@
--- Add migration script here
-
-CREATE TABLE `test` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `test_enum` enum('a','b','c') NOT NULL DEFAULT 'a',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
