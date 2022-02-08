64ca94d7ef5c330d5953cdbde32eefa1ff74aea2
jo_kil <jo.kil.plays@gmail.com>
2022-02-08 20:23:56 +0100

    auth preflag now returns correct response code when auth header is missing

--- plotsystem_api/src/auth/auth_preflag_request_guard.rs
+++ plotsystem_api/src/auth/auth_preflag_request_guard.rs
@@ -1,5 +1,4 @@
 use rocket::http::Status;
-use rocket::outcome::IntoOutcome;
 use rocket::outcome::Outcome;
 use rocket::request::{self, FromRequest, Request};
 use sea_orm_rocket::Connection;
@@ -14,11 +13,7 @@ pub struct AuthPreflag(String);
 #[derive(Debug)]
 pub enum PreflagError {
     Missing,
-    Invalid,
-}
-
-fn is_valid(key: &str) -> bool {
-    key == "valid_key"
+    Unauthorized,
 }
 
 // #[rocket::async_trait]
@@ -27,7 +22,15 @@ impl<'r> FromRequest<'r> for AuthPreflag {
     type Error = PreflagError;
 
     async fn from_request(req: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
-        let key: String = req.headers().get("authorisation").collect::<Vec<&str>>()[0].to_string();
+        let key = match req
+            .headers()
+            .get("authorization")
+            .collect::<Vec<&str>>()
+            .get(0)
+        {
+            Some(key) => key.to_string(),
+            None => return Outcome::Failure((Status::BadRequest, PreflagError::Missing)),
+        };
 
         let db = req
             .guard::<Connection<'_, Db>>()
@@ -37,7 +40,7 @@ impl<'r> FromRequest<'r> for AuthPreflag {
 
         return match crate::db_get::api_keys::api_key_exists(db, &key).await {
             true => Outcome::Success(AuthPreflag(key)),
-            false => Outcome::Failure((Status::Unauthorized, PreflagError::Invalid)),
+            false => Outcome::Failure((Status::Unauthorized, PreflagError::Unauthorized)),
         };
     }
 }
